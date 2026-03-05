1a30e1ead6e6cb61bcd12b6f4cd2e61020d551df
RajniJambhulkar <rajnij2001@gmail.com>
2026-03-05 12:10:42 +0530

    added gitignore file

--- SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765730638b61c1f632e26e70b96a87e5404e9def")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
