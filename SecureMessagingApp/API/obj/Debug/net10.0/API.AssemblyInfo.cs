eb9ea72fe79d8f81a362c6a592cc6e41cc569df5
RajniJambhulkar <rajnij2001@gmail.com>
2026-03-26 00:47:12 +0530

    added the auth and login guard

--- SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7a70df4b27ef480ef11fef6a47383c313ef787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1db41b304fe68589507a78c09c1beb22caad40")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
