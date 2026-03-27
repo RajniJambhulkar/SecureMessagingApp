badbd612d574216b45064bf1378ca8acd0abb848
RajniJambhulkar <rajnij2001@gmail.com>
2026-03-27 09:26:51 +0530

    added chat ui

--- SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ SecureMessagingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1db41b304fe68589507a78c09c1beb22caad40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9ea72fe79d8f81a362c6a592cc6e41cc569df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
